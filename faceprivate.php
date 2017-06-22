38bfea9021b2fb3d600e2e22905ac0dbab9c5c20
italoangelo13 <italoangelo.ti@gmail.com>
2017-06-22 12:53:53 -0300

    novos codigos

---
+++ faceprivate.php
@@ -0,0 +1,51 @@
+<?php
+    
+    $nome = $_REQUEST['nome'];
+    $turma = $_REQUEST['turma'];
+    $serie = $_REQUEST['serie'];
+    $postagem = $_REQUEST['postagem'];
+    $fotos = $_REQUEST['fotos'];
+    $videos = $_REQUEST['videos'];
+    $pessoais = $_REQUEST['pessoais'];
+    $paginas = $_REQUEST['paginas'];
+    $amigos = $_REQUEST['amigos'];
+    
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FacePrivate</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="assets/css/estilo.css" />
+    </head>
+    <body>
+        <div class="container-fluid">
+            <header class="row cabecalho">
+                <div class="col-lg-4">
+                    <img src="assets/img/faceprivate.png" width="50%" alt="FacePrivate" title="FacePrivate"/>  
+                </div>
+            </header>
+            
+            
+            
+            
+            
+            
+            
+            <footer class="row" style="background: #3b5998; padding: 10px; margin-top: 6%; color: #fff;">
+                
+                <div class="col-lg-12">
+                    <address>
+                        <h5>Central de Informatica</h5>
+                        <h5>
+                            <p>Rua Kaquende, 168, Centro, Sabará - MG</p>
+                            <p>(31) 3671-4333</p>
+                        </h5>
+                    </address>
+                </div>
+            </footer>
+        </div>
+    </body>
+</html>
